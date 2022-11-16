dce005c769fc5b6a4eecd7763daa89a7135bb68b
walter <walter.98.kong@gmail.com>
2022-11-17 04:15:57 +0800

    Add peterson's algo and tests

--- src/sync/peterson.rs
+++ src/sync/peterson.rs
@@ -1,47 +1,165 @@
 use super::{Mutex, MutexGuard};
-use std::sync::atomic::AtomicBool;
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc,
+};
 
 #[derive(Default)]
 pub struct Peterson {
-    is_acquired: AtomicBool,
+    a_wants: AtomicBool,
+    b_wants: AtomicBool,
+    a_turn: AtomicBool,
 }
+pub struct PetersonA(Arc<Peterson>);
 
-impl Peterson {
-    pub fn new() -> Self {
-        Self::default()
+pub struct PetersonB(Arc<Peterson>);
+
+impl PetersonA {
+    pub fn new(p: &Arc<Peterson>) -> Self {
+        Self(p.clone())
     }
 }
-impl Mutex for Peterson {
+impl PetersonB {
+    pub fn new(p: &Arc<Peterson>) -> Self {
+        Self(p.clone())
+    }
+}
+impl Mutex for PetersonA {
     type Output = Self;
     fn acquire(&self) -> MutexGuard<Self::Output> {
+        self.0.a_wants.store(true, Ordering::SeqCst);
+        self.0.a_turn.store(false, Ordering::SeqCst);
+        while self.0.b_wants.load(Ordering::SeqCst) && !self.0.a_turn.load(Ordering::SeqCst) {
+            std::hint::spin_loop()
+        }
         MutexGuard { mutex: self }
     }
-
     fn release(&self) {
-        
+        self.0.a_wants.store(false, Ordering::SeqCst)
+    }
+}
+impl Mutex for PetersonB {
+    type Output = Self;
+    fn acquire(&self) -> MutexGuard<Self::Output> {
+        self.0.b_wants.store(true, Ordering::SeqCst);
+        self.0.a_turn.store(true, Ordering::SeqCst);
+        while self.0.a_wants.load(Ordering::SeqCst) && self.0.a_turn.load(Ordering::SeqCst) {
+            std::hint::spin_loop()
+        }
+        MutexGuard { mutex: self }
+    }
+    fn release(&self) {
+        self.0.b_wants.store(false, Ordering::SeqCst)
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::{sync::Arc, thread::spawn};
-
-    use crate::sync::Mutex;
+    use crate::sync::{
+        peterson::{Peterson, PetersonA, PetersonB},
+        Mutex,
+    };
+    use std::{
+        sync::{
+            atomic::{AtomicI32, Ordering},
+            Arc,
+        },
+        thread::{self},
+    };
 
     #[test]
-    fn test() {
-        let num = 100;
-        let mutex = Arc::new(super::Peterson::new());
-        let threads: Vec<_> = (0..num)
-            .map(|_| {
-                let mutex = mutex.clone();
-                spawn(move || {
-                    let _ = mutex.acquire();
-                })
-            })
-            .collect();
-        for thread in threads {
-            thread.join().unwrap();
+    fn race_condition() {
+        // Concurrent
+        const WORK: i32 = 10_000_000 / 2;
+        let incr = Arc::new(AtomicI32::new(0));
+        let decr = Arc::new(AtomicI32::new(0));
+        let th_a = thread::spawn({
+            let incr = incr.clone();
+            let decr = decr.clone();
+            move || {
+                for _ in 0..WORK {
+                    let i = incr.load(Ordering::Acquire);
+                    let d = decr.load(Ordering::Acquire);
+                    incr.store(i + 1, Ordering::Release);
+                    decr.store(d - 1, Ordering::Release);
+                }
+            }
+        });
+        let th_b = thread::spawn({
+            let incr = incr.clone();
+            let decr = decr.clone();
+            move || {
+                for _ in 0..WORK {
+                    let d = decr.load(Ordering::Acquire);
+                    let i = incr.load(Ordering::Acquire);
+                    decr.store(d - 1, Ordering::Release);
+                    incr.store(i + 1, Ordering::Release);
+                }
+            }
+        });
+        th_a.join().unwrap();
+        th_b.join().unwrap();
+        assert!(incr.load(Ordering::Relaxed) > WORK);
+        assert_ne!(incr.load(Ordering::Relaxed), WORK * 2);
+        assert!(decr.load(Ordering::Relaxed) < -WORK);
+        assert_ne!(decr.load(Ordering::Relaxed), -WORK * 2);
+
+        // Sequential
+        let incr = Arc::new(AtomicI32::new(0));
+        let decr = Arc::new(AtomicI32::new(0));
+        for _ in 0..WORK {
+            let i = incr.load(Ordering::Acquire);
+            let d = decr.load(Ordering::Acquire);
+            incr.store(i + 1, Ordering::Release);
+            decr.store(d - 1, Ordering::Release);
         }
+        for _ in 0..WORK {
+            let d = decr.load(Ordering::Acquire);
+            let i = incr.load(Ordering::Acquire);
+            decr.store(d - 1, Ordering::Release);
+            incr.store(i + 1, Ordering::Release);
+        }
+        assert_eq!(incr.load(Ordering::Relaxed), WORK * 2);
+        assert_eq!(decr.load(Ordering::Relaxed), -WORK * 2);
+    }
+
+    #[test]
+    fn mutual_exclusion() {
+        const WORK: i32 = 10_000_000 / 2;
+        let mu = Arc::new(Peterson::default());
+        let incr = Arc::new(AtomicI32::new(0));
+        let decr = Arc::new(AtomicI32::new(0));
+        let th_a = thread::spawn({
+            let mu = PetersonA::new(&mu);
+            let incr = incr.clone();
+            let decr = decr.clone();
+            move || {
+                for _ in 0..WORK {
+                    let _guard = mu.acquire();
+                    let i = incr.load(Ordering::Acquire);
+                    let d = decr.load(Ordering::Acquire);
+                    incr.store(i + 1, Ordering::Release);
+                    decr.store(d - 1, Ordering::Release);
+                }
+            }
+        });
+        let th_b = thread::spawn({
+            let mu = PetersonB::new(&mu);
+            let incr = incr.clone();
+            let decr = decr.clone();
+            move || {
+                for _ in 0..WORK {
+                    let _guard = mu.acquire();
+                    let d = decr.load(Ordering::Acquire);
+                    let i = incr.load(Ordering::Acquire);
+                    decr.store(d - 1, Ordering::Release);
+                    incr.store(i + 1, Ordering::Release);
+                }
+            }
+        });
+        th_a.join().unwrap();
+        th_b.join().unwrap();
+        assert_eq!(incr.load(Ordering::Acquire), WORK * 2);
+        assert_eq!(decr.load(Ordering::Acquire), -WORK * 2);
     }
 }
